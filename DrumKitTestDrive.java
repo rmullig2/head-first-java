02b2e103367fbe9e4bd7999cb72ce2044638b4e2
Ray Mulligan <rmulligan@upstairs.localdomain>
2015-12-05 20:43:55 -0500

    Chapter Two

---
+++ DrumKitTestDrive.java
@@ -0,0 +1,16 @@
+class DrumKitTestDrive {
+  public static void main(String [] args) {
+
+    DrumKit d = new DrumKit();
+    d.playSnare();
+    d.snare = false;
+    d.playTopHat();
+
+    if (d.snare == true) {
+      d.playSnare();
+    }
+  }
+}
+
+// prints bang bang ba-bang
+// prints ding ding da-ding
